ba1140e053ed9540a17b0823f237ff7318d93b8e
Allan Booz <playgame2@hotmail.fr>
2019-03-30 10:10:58 +0400

    fonction update(), collision()

--- app/src/main/java/com/example/entrainement2/SurfaceViewThread.java
+++ app/src/main/java/com/example/entrainement2/SurfaceViewThread.java
@@ -8,12 +8,13 @@ import android.graphics.Rect;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+
 public class SurfaceViewThread extends SurfaceView implements SurfaceHolder.Callback, Runnable {
 
     private SurfaceHolder surfaceHolder = null;
 
     private Paint paint = null;
-    private Paint pBriques = new Paint();
 
     private Thread thread = null;
 
@@ -28,32 +29,125 @@ public class SurfaceViewThread extends SurfaceView implements SurfaceHolder.Call
 
     private Cercle cercle;
 
-    private Briques[] bricks = new Briques[200];
+    private Brique[] bricks = new Brique[200];
+
+    private int nbBricks = 0;
 
     private int brickWidth;
+
     private int brickHeight;
-    private int nbBricks = 0;
+
+    private int distance, cercleDx, cercleDy;
+
     public SurfaceViewThread(Context context){
         super(context);
-
-        //setFocusable(true);
-        setBackgroundColor(Color.BLACK);
         // Get SurfaceHolder object.
         surfaceHolder = this.getHolder();
+
         // Add current object as the callback listener.
         surfaceHolder.addCallback(this);
-        // Create the paint object which will draw the text.
+
         paint = new Paint();
         paint.setColor(Color.GREEN);
 
         cercle = new Cercle(screenWidth+100,screenHeight+400,55,25);
         //cercle.setSpeed(20);
-        pBriques.setColor(Color.BLACK);
-        createBricksAndRestart();
-
         // Set the SurfaceView object at the top of View object.
         setZOrderOnTop(true);
+
+        createBricksAndRestart();
+
     }
+
+    @Override
+    public void run() {
+        createBricksAndRestart();
+        while(threadRunning){
+
+            long startTime = System.currentTimeMillis();
+
+
+
+            cercle.move(this);
+            update();
+            draw();
+
+            try {
+                Thread.sleep(1);
+            }catch (InterruptedException ex){}
+        }
+    }
+
+    public boolean intersect(Cercle c, Brique b){
+        cercleDx = c.getX() - b.getX() - b.getWidth()/2;
+        cercleDy = c.getY() - b.getY() - b.getHeight()/2;
+
+        if(cercleDx > (b.getWidth()/2 + c.getDiametre())){return false;}
+        if(cercleDy > (b.getHeight()/2 + c.getDiametre())){return false;}
+
+        if(cercleDx <= (b.getWidth()/2 )){return false;}
+        if(cercleDy <= (b.getHeight()/2 )){return false;}
+
+        distance = (cercleDx - b.getWidth()/2)^2 + (cercleDy - b.getHeight()/2)^2;
+        return(distance <= (c.getDiametre()^2));
+    }
+
+    public void update(){
+        // Check for ball colliding with a brick
+        for (int i = 0; i < nbBricks; i++) {
+            if (bricks[i].getVisibility()) {
+                if (intersect(cercle, bricks[i])) {
+                    bricks[i].setInvisible();
+                    System.out.print("invisible");
+                }
+            }
+        }
+    }
+    public void createBricksAndRestart() {
+
+        brickWidth = screenWidth / 8;
+
+        brickHeight = screenHeight / 15;
+
+        // Build a wall of bricks
+        nbBricks = 0;
+        for (int column = 0; column < 8; column++) {
+            for (int row = 0; row < 3; row++) {
+                bricks[nbBricks] = new Brique(column, row, brickWidth, brickHeight);
+                nbBricks++;
+            }
+        }
+    }
+
+    private void draw(){
+        int margin = 10000;
+        int left = 0;
+        int top = 0;
+        int right = screenWidth;
+        int bottom = screenHeight;
+        Rect fond = new Rect(left, top, right, bottom);
+
+        canvas = surfaceHolder.lockCanvas();
+
+        // Draw the specify canvas background color.
+        Paint background = new Paint();
+        background.setColor(Color.BLACK);
+
+        canvas.drawRect(fond, background);
+
+
+        for (int i = 0; i < nbBricks; i++) {
+            if (bricks[i].getVisibility()) {
+                paint.setColor(Color.WHITE);
+                canvas.drawRect(bricks[i].getRect(), paint);
+            }
+        }
+        cercle.draw(canvas);
+
+        // Send message to main UI thread to update the drawing to the main view special area.
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         // Create the child thread when SurfaceView is created.
@@ -78,90 +172,5 @@ public class SurfaceViewThread extends SurfaceView implements SurfaceHolder.Call
         // Set thread running flag to false when Surface is destroyed.
         // Then the thread will jump out the while loop and complete.
         threadRunning = false;
-    }
-
-    @Override
-    public void run() {
-        while(threadRunning){
-            long startTime = System.currentTimeMillis();
-
-            cercle.move(this);
-            draw();
-            //long endTime = System.currentTimeMillis();
-
-            //long deltaTime = endTime - startTime;
-            try {
-                Thread.sleep(1/10000);
-            }catch (InterruptedException ex){
-            }
-            /*if(deltaTime < 1){
-                try {
-                    Thread.sleep(1 - deltaTime);
-                }catch (InterruptedException ex){
-                }
-
-            }*/
-        }
-    }
-    public void createBricksAndRestart() {
-
-        brickWidth = screenWidth / 8;
-        brickHeight = screenHeight / 10;
-
-        // Build a wall of bricks
-        nbBricks = 0;
-        for (int column = 0; column < 8; column++) {
-            for (int row = 0; row < 3; row++) {
-                bricks[nbBricks] = new Briques(row, column, brickWidth, brickHeight);
-                nbBricks++;
-            }
-        }
-    }
-
-    public void collision(){
-        for(int i=0;i<bricks.length;i++){
-            if( (cercle.getX() >= bricks[i].getRect().left && cercle.getX() <= bricks[i].getRect().right) &&
-                    ( cercle.getY() >= bricks[i].getRect().top && cercle.getY() <= bricks[i].getRect().bottom)){
-                bricks[i].setInvisible();
-            }
-        }
-    }
-    private void draw(){
-        int margin = 10000;
-
-        int left = 0;
-
-        int top = 0;
-
-        int right = screenWidth;
-
-        int bottom = screenHeight;
-
-        Rect rect = new Rect(left, top, right, bottom);
-
-
-        // Only draw text on the specified rectangle area.
-        canvas = surfaceHolder.lockCanvas();
-
-        // Draw the specify canvas background color.
-        Paint backgroundPaint = new Paint();
-        backgroundPaint.setColor(Color.WHITE);
-        canvas.drawRect(rect, backgroundPaint);
-
-        paint.setColor(Color.BLUE);
-        createBricksAndRestart();
-
-        //canvas.drawRect(100, screenHeight/8, screenWidth-100, screenHeight/2, paint );
-        for (int i = 0; i < nbBricks; i++) {
-            if (bricks[i].getVisibility()) {
-                canvas.drawRect(bricks[i].getRect(), pBriques);
-            }
-        }
-
-
-        cercle.draw(canvas);
-
-        // Send message to main UI thread to update the drawing to the main view special area.
-        surfaceHolder.unlockCanvasAndPost(canvas);
     }
 }
